03a2f50cf4a6179faef4dcb21b45fba8f08a9956
Admin <perchey.loic@gmail.com>
2022-12-22 14:20:32 +0100

    add the base 2

--- src/main/java/com/example/tp1_spring/entity/Todo.java
+++ src/main/java/com/example/tp1_spring/entity/Todo.java
@@ -1,7 +1,18 @@
 package com.example.tp1_spring.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Todo {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String titre;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    private boolean etat;
 }
---
+++ src/main/java/com/example/tp1_spring/tools/ServiceHibernate.java
@@ -0,0 +1,24 @@
+package com.example.tp1_spring.tools;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceHibernate {
+
+    private Session _session;
+
+    public ServiceHibernate() {
+        try {
+            _session = HibernateUtil.getSessionFactory().openSession();
+        }catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Session getSession() {return HibernateUtil.getSessionFactory().openSession();}
+
+}
+
+
